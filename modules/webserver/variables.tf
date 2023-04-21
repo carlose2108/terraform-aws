e1babf8bf53e357a250499f54bed638c56d1d461
Carlos Eduardo Ochoa <charles0658@gmail.com>
2023-04-20 21:06:40 -0400

    Feature/modules (#2)
    * Modularize Terraform Project
    
    * locking feature added

---
+++ modules/webserver/variables.tf
@@ -0,0 +1,8 @@
+variable vpc_id {}
+variable env_prefix {}
+variable image_name {}
+variable my_ip {}
+variable subnet_id {}
+variable instance_type {}
+variable public_key_location{}
+variable avail_zone {}
